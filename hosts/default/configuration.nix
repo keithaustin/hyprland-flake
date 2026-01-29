a9637df33c27d3cdb6927a2fd3c2196d53d362f4
keithaustin <keith@keithaustin.dev>
2026-01-28 21:37:25 -0500

    Remove inputs arg from config for real

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -79,8 +79,6 @@
     programs.hyprland = {
         enable = true;
         xwayland.enable = true;
-        package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-        portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     };
 
     # Env
