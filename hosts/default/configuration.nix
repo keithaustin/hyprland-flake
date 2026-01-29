9f17d950bf881fce8749e9e712bd823a9225542d
keithaustin <keith@keithaustin.dev>
2026-01-28 19:50:30 -0500

    Hopefully fix config.nix

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -6,56 +6,6 @@
         inputs.home-manager.nixosModules.default
     ];
 
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
-
-    networking.hostName = "keith-desktop-nix"
-    networking.networkmanager.enable = true;
-
-    time.timeZone = "America/New_York";
-
-    i18n.defaultLocale = "en_US.UTF-8";
-    i18n.extraLocaleSettings = {
-        LC_ADDRESS = "en_US.UTF-8";
-        LC_IDENTIFICATION = "en_US.UTF-8";
-        LC_MEASUREMENT = "en_US.UTF-8";
-        LC_MONETARY = "en_US.UTF-8";
-        LC_NAME = "en_US.UTF-8";
-        LC_NUMERIC = "en_US.UTF-8";
-        LC_PAPER = "en_US.UTF-8";
-        LC_TELEPHONE = "en_US.UTF-8";
-        LC_TIME = "en_US.UTF-8";
-    };
-
-    services.xserver.xkb = {
-        layout = "us";
-        variant = "";
-    };
-
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-    system.autoUpgrade.enable = true;
-    system.autoUpgrade.dates = "weekly";
-
-    nix.gc.automatic = true;
-    nix.gc.dates = "daily";
-    nix.gc.options = "--delete-older-than 7d";
-    nix.settings.auto-optimise-store = true;
-
-    services.openssh = {
-        enable = true;
-        settings.PermitRootLogin = "no";
-    };
-
-    users.users.keith = {
-        isNormalUser = true;
-        description = "Keith Austin";
-        extraGroups = [ "networkmanager" "wheel" ];
-        packages = with pkgs; [];
-    };
-
-    nixpkgs.config.allowUnfree = true;
-
     home-manager = {
         extraSpecialArgs = { inherit inputs; };
         users.keithaustin = {      
@@ -65,6 +15,4 @@
 
     programs.hyprland.enable = true;
     programs.hyprland.package = inputs.hyprland.packages."${pkgs.system}".hyprland;
-
-    system.stateVersion = "25.11";
 }
