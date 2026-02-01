e50258a4e71d32017f370d21faf2e750fe5fd913
keithaustin <keith@keithaustin.dev>
2026-02-01 18:10:17 -0500

    Move wallpapers into config

--- hosts/precision/home.nix
+++ hosts/precision/home.nix
@@ -58,7 +58,7 @@
 
   xdg.configFile."kitty".source = ../../home/keith/config/kitty;
   xdg.configFile."hypr".source = ../../home/keith/config/hypr;
-  xdg.configGile."wallpapers".source = ../../home/keith/config/wallpapers;
+  xdg.configFile."wallpapers".source = ../../home/keith/config/wallpapers;
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
