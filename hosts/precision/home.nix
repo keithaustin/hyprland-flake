cdec84e4e4c34314e17c46333d2d081d64b241b4
keithaustin <keith@keithaustin.dev>
2026-02-02 15:34:17 -0500

    Add vesktop theme

--- hosts/precision/home.nix
+++ hosts/precision/home.nix
@@ -67,6 +67,7 @@
   xdg.configFile."hypr".source = ../../home/keith/config/hypr;
   xdg.configFile."waybar".source = ../../home/keith/config/waybar;
   xdg.configFile."mako".source = ../../home/keith/config/mako;
+  xdg.configFile."vesktop".source = ../../home/keith/config/vesktop;
   xdg.configFile."wallpapers".source = ../../home/keith/config/wallpapers;
 
   # Let Home Manager install and manage itself.
