54876ea7ed087160f24c50c9fc4c90533cc66e62
keithaustin <keith@keithaustin.dev>
2026-02-01 21:13:17 -0500

    Fix audio config

--- hosts/precision/configuration.nix
+++ hosts/precision/configuration.nix
@@ -71,9 +71,8 @@
 
     services.pipewire = {
         enable = true;
-
         alsa.enable = true;
-        alsa.enable32Bit = true;
+        alsa.support32Bit = true;
         pulse.enable = true;
         jack.enable = true;
         wireplumber.enable = true;
